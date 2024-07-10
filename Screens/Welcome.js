54fc0c987c132ccbaf4474a0903687102440f473
zeba <zebakhan0316@gmail.com>
2024-07-10 14:14:44 +0530

    navigation added to welcome

--- Screens/Welcome.js
+++ Screens/Welcome.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Login from './Login';
 
 const WelcomeScreen = () => {
+    // navigation hook
+    const navigation =useNavigation();
+    useEffect(()=>{
+        setTimeout(()=>{
+        navigation.navigate("Login");
+        },2000)
+
+    },[])
+
     return (
         <View style={{ flex: 1, backgroundColor: "rgba(29,20,21,1)", justifyContent: "center", alignItems: "center" }}>
             <Image source={require('../assets/welcomelogo.png')} style={{ width: 200, height: 200, resizeMode: "contain",opacity:0.8 }} />

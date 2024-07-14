ee6f3964d4a4d48f417d10b9e019f56ac621e35e
Raza <razasfs2003@gmail.com>
2024-07-14 18:17:15 +0530

    creating api for friend requests

--- Components/UserBlock.js
+++ Components/UserBlock.js
@@ -1,8 +1,9 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect } from 'react'
 // importing icons:
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 const UserBlock = ({ id, name, email, userImage }) => {
     const serverUrl = process.env.EXPO_PUBLIC_SERVERURL;
@@ -10,11 +11,20 @@ const UserBlock = ({ id, name, email, userImage }) => {
     useEffect(() => {
 
     }, [])
+    const fetchData = async () => {
+        try {
+            const res = axios.get("")
+        }
+        catch (err) {
+            console.log("error in sending friend req", err);
+            Alert.alert("Failed to send request", "Check your connection");
+        }
+    }
     return (
         <View style={{ minHeight: 60, width: "100%", flexDirection: 'row', justifyContent: "flex-end", alignItems: "center", paddingVertical: 7 }}>
             {/* name dp and email */}
             <TouchableOpacity
-                onPress={() => { navigation.navigate("UserProfile", {id,name,email,userImage}) }}
+                onPress={() => { navigation.navigate("UserProfile", { id, name, email, userImage }) }}
                 style={{ flex: 1, height: "100%", flexDirection: 'row', justifyContent: "flex-start", alignItems: "center", gap: 15, marginRight: 15 }}>
                 <View style={{ justifyContent: "center", alignItems: "center", borderWidth: 2, borderColor: "pink", borderRadius: 100, padding: 2.5 }}>
                     <Image style={{ height: 45, width: 45, borderRadius: 100, }} source={{ uri: `${serverUrl}/images/${userImage}` }} />
